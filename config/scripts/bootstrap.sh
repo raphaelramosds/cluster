547faeb48dc7578899298092b5f3953f7be21a55
Raphael Ramos <raphaelramos@geowellex.com>
2024-10-23 08:28:26 -0300

    Log this shit up

--- config/scripts/bootstrap.sh
+++ config/scripts/bootstrap.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
+# For convenience's sake
+LIGHTBLUE_COLOR=$(tput setaf 6)
+GREEN_COLOR=$(tput setaf 2)
+RESET_COLORS=$(tput sgr0)
+INFO="[${LIGHTBLUE_COLOR}INFO${RESET_COLORS}]"
+SUCCESS="[${GREEN_COLOR}INFO${RESET_COLORS}]"
+
 # Start SSH service
 /etc/init.d/ssh start
 
 # Only execute this on master
 if [[ $HOSTNAME == "master" ]]; then
-    
-    # FIXME (please) Avoid formatting HDFS every time the container start
-    printf "Formatting filesystem ... \n"
+
+    printf "${INFO} Formatting filesystem \n"
     hdfs namenode -format -nonInteractive
+    printf "${SUCCESS} Filesystem formated\n"
 
-    printf "Executing start-dfs ... \n"
+    printf "${INFO} Starting DFS\n"
     $HADOOP_HOME/sbin/start-dfs.sh
+    printf "${SUCCESS} DFS started\n"
 
-    printf "Executing start-yarn ... \n"
+    printf "${INFO} Starting Yarn\n"
     $HADOOP_HOME/sbin/start-yarn.sh
+    printf "${SUCCESS} Yarn started\n"
+
+    printf "${INFO} Creating spark logs\n"
+    hdfs dfs -mkdir /spark-logs
+    printf "${SUCCESS} Spark logs created\n"
+
+    printf "${INFO} Sending JAR files to HDFS\n"
+    hdfs dfs -mkdir /spark-libs
+    hdfs dfs -put $HADOOP_HOME/spark/jars/*.jar /spark-libs/
+    printf "${SUCCESS} JAR files sent\n"
+
+    printf "${INFO} Start spark history server\n"
+    $SPARK_home/sbin/start-history-server.sh
+    printf "${SUCCESS} History server launched\n"
 fi
 
 # Starting bash terminal for keeping slaves alive
 /bin/bash
+
+printf "${SUCCESS} Cluster set up\n"
